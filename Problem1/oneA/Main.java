e131a99a56ef2103b890551510ed851cc8915473
aa-ryan <aryansinha6363@gmail.com>
2021-07-19 13:27:00 +0530

    1A~Main added

---
+++ Problem1/oneA/Main.java
@@ -0,0 +1,23 @@
+package oneA;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Student[] str = new Student[10];
+		Scanner sObj = new Scanner(System.in);
+		System.out.println("Enter the number of students");
+		int n = sObj.nextInt();
+
+		for (int i = 1; i <= n; i++) {
+			str[i] = new Student();
+			str[i].get(i);
+		}
+		
+		System.out.println("STUDENT INFORMATION");
+		System.out.println("NAME\t" + "USN\t" + "BRANCH\t" + "PHONE_NUMBER\n");
+		for(int i = 1; i <= n; i++) {
+			str[i].print();
+		}
+		sObj.close();
+	}
+}
