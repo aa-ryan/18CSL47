0317f96a159a21f31abad36d0d5b138ccd3b74f0
aa-ryan <aryansinha6363@gmail.com>
2021-07-19 12:24:19 +0530

    1A~Student

---
+++ Problem1/oneA/Student.java
@@ -0,0 +1,23 @@
+package oneA;
+import java.util.Scanner;
+
+public class Student {
+
+
+	private String usn, name, branch;
+	private long phone;
+
+	Scanner sObj = new Scanner(System.in);
+
+	void get(int x) {
+		System.out.println("Enter NAME, USN, BRANCH and PHONE of a " + x + "student");
+		name = sObj.nextLine();
+		usn = sObj.nextLine();
+		branch = sObj.nextLine();
+		phone = sObj.nextLong();
+	}
+
+	void print() {
+		System.out.println(name + "\t" + usn  + "\t" + branch + "\t" + phone + "\n");
+	}
+}
